b946cbb2951f79145289945691e668adc3c5ec77
estudner000 <estudner000@warren.k12.in.us>
2026-04-29 12:39:52 -0400

    initial commit

---
+++ Found1/core/src/main/java/io/github/found1/Main.java
@@ -0,0 +1,11 @@
+package io.github.found1;// ============================================================
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Screen;
+
+public class Main extends Game {
+    public void create() {
+        setScreen(new AnimationDemoScreen());
+
+    }
+}
